347a0dc3673ef9aeae40087c9f954befd638824c
Chathura Prasanga <chathuraprasanga98@gmail.com>
2023-05-18 21:02:46 +0530

    1.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
+
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BestSellerComponent} from './best-seller/best-seller.component';
@@ -15,8 +16,18 @@ import {FlexLayoutModule} from "@angular/flex-layout";
 import {SharedModule} from "./shared/shared.module";
 import {HttpClientModule} from "@angular/common/http";
 import { SignupComponent } from './signup/signup.component';
+import {NgxUiLoaderConfig, NgxUiLoaderModule, SPINNER} from "ngx-ui-loader";
 
-
+const ngxUiLoaderConfig:NgxUiLoaderConfig={
+  text:"Loading...",
+  textColor:"#FFFFFF",
+  textPosition:"center-center",
+  bgsColor:"#7b1fa2",
+  fgsColor:"#7b1fa2",
+  fgsType:SPINNER.squareJellyBox,
+  fgsSize:100,
+  hasProgressBar:false
+}
 
 @NgModule({
   declarations: [
@@ -38,7 +49,7 @@ import { SignupComponent } from './signup/signup.component';
     FlexLayoutModule,
     SharedModule,
     HttpClientModule,
-
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/global-constants.ts
+++ src/app/shared/global-constants.ts
@@ -11,6 +11,6 @@ export class GlobalConstants {
   public static contactNumberRegex:string = "^[e0-9]{10,10}$";
 
   //Variable
-  public static error:String = "error";
+  public static error:any = "error";
 
 }
