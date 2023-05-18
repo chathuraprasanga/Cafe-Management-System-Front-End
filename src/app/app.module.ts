1c7f2518a5650a19daef2bbb373d8ee6d6d45449
Chathura Prasanga <chathuraprasanga98@gmail.com>
2023-05-18 10:04:30 +0530

    1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BestSellerComponent} from './best-seller/best-seller.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {HomeComponent} from './home/home.component';
+import {SharedComponent} from './shared/shared.component';
+import {FullComponent} from './layouts/full/full.component';
+import {HeaderComponent} from './layouts/full/header/header.component';
+import {SidebarComponent} from './layouts/full/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BestSellerComponent,
+    DashboardComponent,
+    HomeComponent,
+    SharedComponent,
+    FullComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
