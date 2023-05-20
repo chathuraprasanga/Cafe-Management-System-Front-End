3ba333dfb35492223172417611a993dca2a7b7b4
Chathura Prasanga <chathuraprasanga98@gmail.com>
2023-05-20 16:37:20 +0530

    2.1 login done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,44 +5,48 @@ import {MatDialogRef} from "@angular/material/dialog";
 import {NgxUiLoaderService} from "ngx-ui-loader";
 import {SnackbarService} from "../services/snackbar.service";
 import {GlobalConstants} from "../shared/global-constants";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-forgot-password',
-  templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
-export class ForgotPasswordComponent implements OnInit{
+export class LoginComponent implements OnInit{
 
-  forgotPasswordForm:any = FormGroup;
+  hide=true;
+  loginForm:any = FormGroup;
   responseMessage:any;
 
   constructor(
-    private formBuilder:FormBuilder,
+    private formBuilder : FormBuilder,
     private userService : UserService,
-    private dialogRef : MatDialogRef<ForgotPasswordComponent>,
-    private ngxService:NgxUiLoaderService,
-    private snackbarService : SnackbarService
-  ){ }
+    private dialogRef : MatDialogRef<LoginComponent>,
+    private  ngxService : NgxUiLoaderService,
+    private snackbarService : SnackbarService,
+    private router : Router
+  ) { }
 
   ngOnInit(): void {
-    this.forgotPasswordForm = this.formBuilder.group({
-      email:[null,[Validators.required,Validators.pattern(GlobalConstants.emailRegex)]]
-    });
+    this.loginForm = this.formBuilder.group({
+      email:[null,[Validators.required,Validators.pattern(GlobalConstants.emailRegex)]],
+      password:[null,[Validators.required]]
+    })
   }
 
   handleSubmit(){
     this.ngxService.start();
-    var formData = this.forgotPasswordForm.value;
+    var formData = this.loginForm.value;
     var data = {
-      email:formData.email
+      email: formData.email,
+      password : formData.password
     };
-    this.userService.forgotPassword(data).subscribe((response:any)=>{
+    this.userService.login(data).subscribe((response:any)=>{
       this.ngxService.stop();
-      this.responseMessage = response?.message;
       this.dialogRef.close();
-      this.snackbarService.openSnackBar(this.responseMessage,"");
+      localStorage.setItem('token',response.token);
+      this.router.navigate(['/cafe/dashboard']);
     },(error)=>{
-      this.ngxService.stop();
       if (error.error?.message){
         this.responseMessage = error.error?.message;
       }else {
